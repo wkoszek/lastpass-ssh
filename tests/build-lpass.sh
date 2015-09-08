05c971a27c03a8a0517bfbf31921dcb6e8a64934
Wojciech A. Koszek <wojciech@koszek.com>
2015-09-08 02:21:52 -0700

    Fix travis a bit more: try to call lpass with proper paths.

--- tests/build-lpass.sh
+++ tests/build-lpass.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/lastpass/lastpass-cli.git
 cd lastpass-cli
-make PREFIX=${HOME}/bin -j 16
-make PREFIX=${HOME}/bin install
+make PREFIX=${HOME}/lpass -j 16
+make PREFIX=${HOME}/lpass install
